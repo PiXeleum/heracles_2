16e6934954113e1a77e0c5f7a0bbd35daaa051d0
PiXeleum <jeffreyrigaux@gmail.com>
2022-04-07 11:36:28 +0200

    finish

--- src/Fighter.js
+++ src/Fighter.js
@@ -6,18 +6,35 @@ class Fighter {
         this.strength = strength;
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
+        this.weapon = null;
+        this.shield = null;
     }
 
 
     // Launch a fight
     fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+        const attackPoints = this.getRandomInt(this.getDamage());
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+        const damages = Math.max(attackPoints - defender.getDefense(), 0);
 
         defender.life = Math.max(defender.life - damages, 0);
     }
 
+    getDamage(){
+        if(this.weapon != null) {
+            return (this.strength + this.weapon.damage);
+        } else {
+            return (this.strength)
+        }
+    }
+
+    getDefense(){
+        if(this.shield != null) {
+            return (this.dexterity + this.shield.protection);
+        } else {
+            return (this.dexterity)
+        }
+    }
 
     // Generate a random value between 1 and max
     getRandomInt(max) {
@@ -30,5 +47,6 @@ class Fighter {
         return this.life > 0;
     }
 }
+
 
 module.exports = Fighter;
